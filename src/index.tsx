72e618051ce9f5cc5b412af9f242178b430d3bd0
dqhl76 <dqhl76@gmail.com>
2022-09-14 11:07:57 +0800

    feat: add the component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Editor from './Editor/editor';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Editor/>
     </React.StrictMode>,
 );
 
