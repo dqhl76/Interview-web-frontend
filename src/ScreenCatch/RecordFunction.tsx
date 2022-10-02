79a45519a5a8f66273601244e1ff3abb801ea24a
apexnewbie <106024759+apexnewbie@users.noreply.github.com>
2022-10-02 22:47:55 +0800

    feat: finishe webrtc part
    * feat: all functions of screen catch are completed !!!!!!
    
    * feat: all functions of screen catch are completed !!!!!!
    
    Co-authored-by: TianyuWu <tianyu.wu1@ucdconnect.ie>

--- src/ScreenCatch/RecordFunction.tsx
+++ src/ScreenCatch/RecordFunction.tsx
@@ -1,9 +1,38 @@
 export function captureUserMedia(callback: any) {
-    var params = { audio: true, video: true};
-    console.log(1)
-    navigator.mediaDevices.getDisplayMedia(params).then(callback, (error) => {
-        alert(JSON.stringify(error));
-    });
+    var params = { audio: true, video: true };
+    var mic = { audio: true, video: false };
+    console.log(1);
+    navigator.mediaDevices.getDisplayMedia(params).then(
+        (videoStream) => {
+            navigator.mediaDevices.getUserMedia(mic).then(
+                (micStream) => {
+                    var context = new AudioContext();
+                    var baseSource =
+                        context.createMediaStreamSource(videoStream);
+                    var extraSource =
+                        context.createMediaStreamSource(micStream);
+                    var dest = context.createMediaStreamDestination();
+                    var baseGain = context.createGain();
+                    var extraGain = context.createGain();
+                    baseGain.gain.value = 0.8;
+                    extraGain.gain.value = 0.8;
+                    baseSource.connect(baseGain).connect(dest);
+                    extraSource.connect(extraGain).connect(dest);
+                    var result = new MediaStream([
+                        videoStream.getVideoTracks()[0],
+                        dest.stream.getAudioTracks()[0],
+                    ]);
+                    callback(result);
+                },
+                (error) => {
+                    alert(JSON.stringify(error));
+                },
+            );
+        },
+        (error) => {
+            alert(JSON.stringify(error));
+        },
+    );
 }
 
 export function getFileName() {
