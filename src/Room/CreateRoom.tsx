093d206c967b2ac39cdf398e9f3d2379d31b7ac7
dqhl76 <dqhl76@gmail.com>
2022-12-06 21:05:06 +0800

    feat: handle login register rooms functions (#17)

--- src/Room/CreateRoom.tsx
+++ src/Room/CreateRoom.tsx
@@ -3,20 +3,75 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Rooms.css';
-import { CloseButton, Container, Row } from 'react-bootstrap';
+import { Alert, CloseButton, Container, Row } from 'react-bootstrap';
+import axios from 'axios';
 
-function CreateRoom() {
-    const Time = () => {
-        let st = document.getElementById('startTime') as HTMLInputElement;
-        let dur = document.getElementById('endTime') as HTMLInputElement;
+interface IPros {
+    closeSelf: () => void;
+}
 
-        let startTime: String = st.value;
-        let duration: String = dur.value;
-    };
+const CreateRoom: React.FC<IPros> = (props) => {
+    const { closeSelf } = props;
     const close = () => {
-        let window = document.getElementById('create') as HTMLElement;
-        window.style.display = 'none';
+        closeSelf();
     };
+
+    const handleSubmit = () => {
+        if (email == '' || date == '' || startTime == '' || duration == '') {
+            setIsAlert(true);
+            setAlertMessage('Please fill all the fields');
+            setVariant('danger');
+            return;
+        }
+        const time = date + 'T' + startTime + ':00.000Z';
+        axios
+            .post(
+                'https://socket.realdqhl.com/create_room',
+                {
+                    start: time,
+                    duration: duration,
+                    interviewed: email,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization:
+                            'Bearer ' + localStorage.getItem('token'),
+                    },
+                },
+            )
+            .then((response) => {
+                console.log(response);
+                if (response.data.status == 'false') {
+                    setIsAlert(true);
+                    setAlertMessage(response.data.message);
+                    setVariant('danger');
+                    return;
+                }
+                const data = response.data.data;
+                setIsAlert(true);
+                setAlertMessage(
+                    `You have successfully planned an interview, the meeting is scheduled at ${data.start}, the duration is ${data.duration} minutes, and the interview room id is ${data.room_id}`,
+                );
+                setVariant('success');
+            })
+            .catch((error) => {
+                if (error.response.status === 422) {
+                    setIsAlert(true);
+                    setAlertMessage(error.response.data.message);
+                    setVariant('danger');
+                }
+            });
+    };
+
+    const [isAlert, setIsAlert] = React.useState<boolean>(false);
+    const [alertMessage, setAlertMessage] = React.useState<string>('');
+    const [variant, setVariant] = React.useState<string>('danger');
+
+    const [email, setEmail] = React.useState<string>('');
+    const [date, setDate] = React.useState<string>('');
+    const [startTime, setStartTime] = React.useState<string>('');
+    const [duration, setDuration] = React.useState<string>('');
 
     return (
         <div className={'bordlist container cr'} id={'create'}>
@@ -27,7 +82,12 @@ function CreateRoom() {
             <Form>
                 <Form.Group className='mb-3' controlId='formBasicEmail'>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type='email' placeholder='Enter email' />
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                     <Form.Text className='text-muted'>
                         Please Enter Another Attendee's Email..
                     </Form.Text>
@@ -35,22 +95,42 @@ function CreateRoom() {
 
                 <Form.Group className='mb-3'>
                     <Form.Label>Date</Form.Label>
-                    <Form.Control type='date' />
+                    <Form.Control
+                        type='date'
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group className='mb-3'>
                     <Form.Label>Start Time</Form.Label>
-                    <Form.Control type='Time' id={'startTime'} />
+                    <Form.Control
+                        type='Time'
+                        id={'startTime'}
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group className='mb-3'>
-                    <Form.Label>End Time(min)</Form.Label>
-                    <Form.Control type='Number' id={'endTime'} />
+                    <Form.Label>Duration(min)</Form.Label>
+                    <Form.Control
+                        type='Number'
+                        id={'endTime'}
+                        value={duration}
+                        onChange={(e) => setDuration(e.target.value)}
+                    />
                 </Form.Group>
+                <Alert key={variant} variant={variant} show={isAlert}>
+                    {alertMessage}
+                </Alert>
                 <Row className={'text-center'}>
                     <Button
                         variant='primary'
                         type='submit'
                         className={'sm'}
-                        onClick={Time}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleSubmit();
+                        }}
                     >
                         Submit
                     </Button>
@@ -58,6 +138,6 @@ function CreateRoom() {
             </Form>
         </div>
     );
-}
+};
 
 export default CreateRoom;
