e1480f446815a0f22cd7975246c4abfa38c51ea6
dqhl76 <dqhl76@gmail.com>
2022-12-06 11:34:19 +0800

    feat: add router (#15)
    * feat: add router
    
    * feat: add router 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    createRoutesFromElements,
+} from 'react-router-dom';
 import './index.css';
 import Record from './ScreenCatch/Record';
 import Editor from './Editor/editor';
-import Login from "./Login/Login";
+import Login from './Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import Webrtc from './Webrtc/Webrtc';
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
+import Header from './Header/Header';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Header />}>
+            <Route index element={<Login />} />
+            <Route path='/home' element={<div></div>}/>
+            <Route path='/room/:id' element={<Editor />} />
+        </Route>,
+    ),
+);
+
 root.render(
     <React.StrictMode>
-        <Webrtc />
-        <Record />
-        <Editor />
+        {/* <Webrtc /> */}
         {/*<Record />*/}
         {/*<Editor />*/}
-
-        <Login/>
+        <RouterProvider router={router} />
+        {/* <Login/> */}
     </React.StrictMode>,
 );
 
